0a1cc85755fb912254c650781923e51a642b3153
yuxilong <305758560@qq.com>
2022-11-15 17:58:43 +0800

    更新版本号

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'pod/command/publish'
+require 'pod/install_clean'
 require 'cocoapods-publish/pod_utils'
 require 'cocoapods-publish/repo_utils'
 require 'cocoapods-publish/gitlab_utils'
