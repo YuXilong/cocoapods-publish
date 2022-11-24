6241b39ee88909b0594aab790510ec26de22faae
yuxilong <305758560@qq.com>
2022-11-24 17:07:02 +0800

    优化切换源地址相关逻辑

--- lib/pod/auto_switch_source_hook.rb
+++ lib/pod/auto_switch_source_hook.rb
@@ -2,46 +2,60 @@
 
 require 'English'
 Pod::HooksManager.register('cocoapods-publish', :pre_install) do |context, _|
-  next unless context.podfile.plugins.keys.include?('cocoapods-publish')
+  cache_root = Pod::Config.instance.cache_root.to_s
+  project_pods_root = Pod::Config.instance.project_pods_root.to_s
+  target_root = "#{cache_root}/Pods"
+  source_cache_root = "#{cache_root}/Pods_Source"
+  framework_cache_root = "#{cache_root}/Pods_Framework"
+  use_framework = ENV['USE_FRAMEWORK'] == '1'
 
-  project_root = "#{Pod::Config.instance.project_root}/Pods"
-  cache_root = "#{Pod::Config.instance.cache_root}/Pods"
-
-  next unless Pod::Config.instance.clean_before_install
-
-  #
-  # use_framework = ENV['USE_FRAMEWORK'] == '1'
-  # if use_framework && !Dir.glob("#{project_root}/**/BT*/**/BT*.framework").empty? && !Dir.glob("#{cache_root}/**/BT*/**/BT*.framework").empty?
-  #   next
-  # end
-  #
-  # if !use_framework && Dir.glob("#{project_root}/**/BT*/**/BT*.framework").empty? && Dir.glob("#{cache_root}/**/BT*/**/BT*.framework").empty?
-  #   next
-  # end
-
-  puts '开始清理缓存...'.yellow
-
-  Dir.glob("#{cache_root}/**/BT*/")
-     .each { |path| `rm -rf #{path}` if Dir.exist?(path) }
-
-  Dir.glob("#{project_root}/**/BT*/")
-     .each { |path| `rm -rf #{path}` if Dir.exist?(path) }
-
-  puts '缓存清理完成'.green
-end
-
-Pod::HooksManager.register('cocoapods-publish', :post_install) do |context, _|
-  project_root = "#{Pod::Config.instance.project_root}/Pods"
-  next if Dir.glob("#{project_root}/**/RCConfig.plist").empty?
-
-  Dir.glob("#{project_root}/**/RCConfig.plist")
-     .each do |file|
-    command = "/usr/libexec/PlistBuddy -c 'Add :Connection dict' #{file}"
-    command += " && /usr/libexec/PlistBuddy -c 'Add :Connection:ForceKeepAlive bool true' #{file} 2>/dev/null"
-    `#{command}`
-    puts 'RCConfig.plist配置项同步成功！'.green if $CHILD_STATUS.exitstatus.zero?
+  # 未启用插件统一走源码
+  unless context.podfile.plugins.keys.include?('cocoapods-publish')
+    if Dir.exist?(source_cache_root)
+      `mv #{target_root} #{framework_cache_root}`
+      `mv #{source_cache_root} #{target_root}`
+    end
+    next
   end
+
+  if !Dir.exist?(source_cache_root) && !Dir.exist?(framework_cache_root)
+    Dir.glob("#{cache_root}/**/BT*/")
+       .each { |path| `rm -rf #{path}` if Dir.exist?(path) }
+    Dir.glob("#{project_pods_root}/**/BT*/")
+       .each { |path| `rm -rf #{path}` if Dir.exist?(path) }
+    if use_framework
+      `cp -r #{target_root} #{source_cache_root}`
+    else
+      `cp -r #{target_root} #{framework_cache_root}`
+    end
+    next
+  end
+
+  next if !Dir.exist?(framework_cache_root) && use_framework
+  next if !Dir.exist?(source_cache_root) && !use_framework
+
+  if use_framework
+    `mv #{target_root} #{source_cache_root}`
+    `mv #{framework_cache_root} #{target_root}`
+  else
+    `mv #{target_root} #{framework_cache_root}`
+    `mv #{source_cache_root} #{target_root}`
+  end
+  puts '已切换源地址'.green
 end
+
+# Pod::HooksManager.register('cocoapods-publish', :post_install) do |context, _|
+#   project_root = "#{Pod::Config.instance.project_root}/Pods"
+#   next if Dir.glob("#{project_root}/**/RCConfig.plist").empty?
+#
+#   Dir.glob("#{project_root}/**/RCConfig.plist")
+#      .each do |file|
+#     command = "/usr/libexec/PlistBuddy -c 'Add :Connection dict' #{file}"
+#     command += " && /usr/libexec/PlistBuddy -c 'Add :Connection:ForceKeepAlive bool true' #{file} 2>/dev/null"
+#     `#{command}`
+#     puts 'RCConfig.plist配置项同步成功！'.green if $CHILD_STATUS.exitstatus.zero?
+#   end
+# end
 
 Pod::HooksManager.register('cocoapods-publish', :source_provider) do |context, _|
   sources_manger = Pod::Config.instance.sources_manager
