d3e22afcee2c07de64dfeb57a628f220e3b5d3b1
yuxilong <305758560@qq.com>
2023-02-07 18:56:18 +0800

    优化切换源时的已知问题

--- lib/pod/auto_switch_source_hook.rb
+++ lib/pod/auto_switch_source_hook.rb
@@ -3,49 +3,52 @@
 require 'English'
 Pod::HooksManager.register('cocoapods-publish', :pre_install) do |context, _|
   cache_root = Pod::Config.instance.cache_root.to_s
+  cache_self_root = "#{Dir.home}/.cocoapods"
   project_pods_root = Pod::Config.instance.project_pods_root.to_s
   target_root = "#{cache_root}/Pods"
-  source_cache_root = "#{cache_root}/Pods_Source"
-  framework_cache_root = "#{cache_root}/Pods_Framework"
+  source_cache_root = "#{cache_self_root}/Pods_Source"
+  framework_cache_root = "#{cache_self_root}/Pods_Framework"
   use_framework = ENV['USE_FRAMEWORK'] == '1'
 
   # 未启用插件统一走源码
   unless context.podfile.plugins.keys.include?('cocoapods-publish')
-    if Dir.exist?(source_cache_root)
-      `mv #{target_root} #{framework_cache_root}`
-      `mv #{source_cache_root} #{target_root}`
-    end
+    `cp -r #{source_cache_root} #{target_root}` if Dir.exist?(source_cache_root)
     next
   end
 
+  # 移除本地项目内缓存
+  Dir.glob("#{project_pods_root}/**/BT*/")
+     .each { |path| `rm -rf #{path}` if Dir.exist?(path) }
+
+  # 初始化缓存
   if !Dir.exist?(source_cache_root) && !Dir.exist?(framework_cache_root)
+    puts '初始化自定义缓存...'.yellow
     Dir.glob("#{cache_root}/**/BT*/")
        .each { |path| `rm -rf #{path}` if Dir.exist?(path) }
-    Dir.glob("#{project_pods_root}/**/BT*/")
-       .each { |path| `rm -rf #{path}` if Dir.exist?(path) }
-    if use_framework
-      `cp -r #{target_root} #{source_cache_root}`
-    else
-      `cp -r #{target_root} #{framework_cache_root}`
-    end
+
+    `cp -r #{target_root} #{source_cache_root}`
+    `cp -r #{target_root} #{framework_cache_root}`
     next
   end
 
-  next if !Dir.exist?(framework_cache_root) && use_framework
-  next if !Dir.exist?(source_cache_root) && !use_framework
+  is_using_framework = Dir.glob("#{cache_root}/**/BT*/**/*.framework").count > 5
+  next if (is_using_framework && use_framework) || (!is_using_framework && !use_framework)
 
+  # 切换仓库，同步缓存
+  puts '正在切换模式，同步缓存...'.yellow
   if use_framework
-    `mv #{target_root} #{source_cache_root}`
-    `mv #{framework_cache_root} #{target_root}`
+    `rm -rf #{source_cache_root}`
+    `cp -r #{target_root} #{source_cache_root}`
+    `rm -rf #{target_root}`
+    `cp -r #{framework_cache_root} #{target_root}`
   else
-    `mv #{target_root} #{framework_cache_root}`
-    `mv #{source_cache_root} #{target_root}`
+    `rm -rf #{framework_cache_root}`
+    `cp -r #{target_root} #{framework_cache_root}`
+    `rm -rf #{target_root}`
+    `cp -r #{source_cache_root} #{target_root}`
   end
 
-  Dir.glob("#{project_pods_root}/BT*").each { |path| `rm -rf #{path}` }
-
-
-  puts '已切换源地址'.green
+  puts "已切换到#{use_framework ? '二进制' : '源码'}模式".green
 end
 
 Pod::HooksManager.register('cocoapods-publish', :source_provider) do |context, _|
