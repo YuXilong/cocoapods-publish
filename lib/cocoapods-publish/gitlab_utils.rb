86588cebecdc9a81acd36a2c966c2b08791f76c0
yuxilong <305758560@qq.com>
2023-03-13 18:57:01 +0800

    修复已知问题

--- lib/cocoapods-publish/gitlab_utils.rb
+++ lib/cocoapods-publish/gitlab_utils.rb
@@ -43,7 +43,9 @@ module Pod
 
         UI.puts '-> 正在关联远程仓库...'.yellow unless @from_wukong
         command = "git branch -M #{default_branch} --quiet"
-        command += ' && git remote remove origin'
+
+        # `git remote get-url --all origin`.to_s
+        # command += ' && git remote remove origin'
         command += " && git remote add origin #{ssh_url}"
         command += ' && git add . && git commit -m "Initial commit" --quiet'
         command += ' && git push origin main --quiet'
