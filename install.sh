869761bb2fbb9c20e44377c2fa922ec7df8f64b4
yuxilong <305758560@qq.com>
2024-02-21 13:48:52 +0800

    update

--- install.sh
+++ install.sh
@@ -13,18 +13,11 @@ url="https://github.com/YuXilong/cocoapods-publish/releases/download/v2.2.0/wuko
 if [ "${arch}" == "x86_64" ]; then
   url="https://github.com/YuXilong/cocoapods-publish/releases/download/v2.2.0/wukong_i386_1.2.2"
 fi
-path=`pwd`
-des="${path}/wukong"
-# 从github下载到/usr/local/bin
-# curl -L "$url" -o /usr/local/bin/wukong
+des="/opt/homebrew/opt/ruby/bin/wukong"
 curl -L "$url" -o $des
-# chmod +x /usr/local/bin/wukong
 chmod +x $des
-
-# cp "$des" /usr/local/bin
-
 echo '- Installation successful!'
 
 # 更新本地cocoapods插件
-./wukong update
-./wukong update --pod-plugins
+wukong update
+wukong update --pod-plugins
