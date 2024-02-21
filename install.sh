3a2150f98e5cb11939f7e360e188a5f6620ae6a4
yuxilong <305758560@qq.com>
2024-02-21 11:26:17 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# We don't need return codes for "$(command)", only stdout is needed.
+# Allow `[[ -n "$(command)" ]]`, `func "$(command)"`, pipes, etc.
+# shellcheck disable=SC2312
+
+set -u
+
+abort() {
+  printf "%s\n" "$@" >&2
+  exit 1
+}
+
+echo '- Installing WuKong...'
+
+arch="$(uname -m)"
+url="https://github.com/YuXilong/cocoapods-publish/releases/download/v2.2.0/wukong_arm64_1.2.2"
+if [ "${arch}" == "x86_64" ]; then
+  url="https://github.com/YuXilong/cocoapods-publish/releases/download/v2.2.0/wukong_i386_1.2.2"
+fi
+
+# 从github下载到/usr/local/bin
+curl -L "$url" -o /usr/local/bin/wukong
+chmod +x /usr/local/bin/wukong
+
+echo '- Installation successful!'
+
+# 更新本地cocoapods插件
+wukong update
+wukong update --pod-plugins
