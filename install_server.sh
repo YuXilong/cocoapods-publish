180ed03871ace61b556884d62c79d47330029d2a
yuxilong <305758560@qq.com>
2024-03-21 17:00:09 +0800

    增加更新操作

--- install_server.sh
+++ install_server.sh
@@ -5,7 +5,7 @@ set -u
 # 安装WuKongServer
 install_wukong_server() {
     # 安装WuKong
-    echo '- Installing WuKongServer...'
+    echo '- Installing wukong-server...'
     url="https://github.com/YuXilong/cocoapods-publish/releases/download/v2.2.0/wukong-server"
     des="/opt/homebrew/bin/wukong-server"
     curl -L "$url" -o $des
@@ -16,3 +16,13 @@ install_wukong_server() {
 if ! command -v wukong-server &> /dev/null; then
     install_wukong_server
 fi
+
+# 提示用户是否继续执行
+read -p "已安装wukong-server，是否更新? (y/n): " choice
+
+# 根据用户的选择决定是否继续执行
+case "$choice" in
+  y|Y ) install_wukong_server;
+  n|N ) exit;;
+  * ) exit;;
+esac
