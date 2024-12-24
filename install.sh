d411d05da5df9a901b6e508f486744dac7052d30
Fish. <yuxilong1006@gmail.com>
2024-12-24 13:03:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ install_wukong() {
     # 安装WuKong
     echo '- Installing WuKong...'
     arch="$(uname -m)"
-    url="https://github.com/YuXilong/cocoapods-publish/releases/download/v2.2.0/wukong_arm64_2.2.0"
+    url="https://vip.123pan.cn/1832538346/14654662"
     if [ "${arch}" == "x86_64" ]; then
     url="https://github.com/YuXilong/cocoapods-publish/releases/download/v2.2.0/wukong_i386_2.2.0"
     fi
